530876800e3b91b16e68639de6270acda046677a
Morgaine <morgainedv@gmail.com>
2020-01-06 15:35:11 +0000

    adds printer paperamount method

---
+++ src/main/java/Printer.java
@@ -0,0 +1,12 @@
+public class Printer {
+
+    private int paperAmount;
+
+    public Printer(int paperAmount){
+        this.paperAmount = paperAmount;
+    }
+
+    public int paperAmount(){
+        return this.paperAmount;
+    }
+}
