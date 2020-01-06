aef49d86d1baec98fd2172a04340dea9ab9bf0ba
Morgaine <morgainedv@gmail.com>
2020-01-06 15:16:03 +0000

    create first test for waterbottle

---
+++ src/test/java/WaterbottleTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterbottleTest {
+
+
+    Waterbottle waterbottle;
+
+    @Before
+    public void before(){
+        waterbottle = new Waterbottle();
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterbottle.getVolume());
+    }
+
+
+}
